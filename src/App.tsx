4f1f5e3909eabb654100a9c0bfa8df59adf82325
George Panov <georgepanov19@gmail.com>
2026-09-14 20:17:54 +0300

    feat: build on pull request

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </div>
         <div>
           <h1>Get started</h1>
+          <h2>Build on pull request is working!</h2>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
