eb93bf17e298c558fbf3a2e4c990d0094505d3e5
George Panov <georgepanov19@gmail.com>
2026-09-17 14:11:51 +0300

    feat: переименовывает проект в echo

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
-  base: '/hbm/',
+  base: '/echo/',
   server: {
     host: true,
     port: 3000,
